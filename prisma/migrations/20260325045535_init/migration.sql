40769c7b036c4bcba29693db4f7188e9f93c1e1d
su-wone <rkdtndnjs02@naver.com>
2026-03-25 13:57:46 +0900

    마이그레이션 실행

---
+++ prisma/migrations/20260325045535_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Memo" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Memo_pkey" PRIMARY KEY ("id")
+);
