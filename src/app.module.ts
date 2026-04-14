2ca7a256d2286f0e08497e2dd26da378ae01172b
su-wone <rkdtndnjs02@naver.com>
2026-04-14 11:17:42 +0900

    VEASLY-430 [BE] Memo/Upload 모듈 제거 및 CardType 확장

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module.js';
-import { MemoModule } from './memo/memo.module.js';
-import { UploadModule } from './upload/upload.module.js';
 import { BoardModule } from './board/board.module.js';
 
 @Module({
-  imports: [PrismaModule, MemoModule, UploadModule, BoardModule],
+  imports: [PrismaModule, BoardModule],
 })
 export class AppModule { }
